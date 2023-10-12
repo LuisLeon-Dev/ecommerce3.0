4eac955d21cb0339a614f114f92029ee14713f19
LuisLeon-Dev <lleon5073@gmail.com>
2023-10-11 23:42:05 -0600

    auth bug fixed

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,58 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: 'userLogged',
+  user: "userLogged",
   updatedAt: Date.now().toLocaleString(),
   total: 50,
   items: [],
-}
+};
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addItem: (state, action) => {
       const productRepeated = state.items.find(
-        item => item.id === action.payload.id
-      )
+        (item) => item.id === action.payload.id
+      );
       if (productRepeated) {
-        const itemsUpdated = state.items.map(item => {
+        const itemsUpdated = state.items.map((item) => {
           if (item.id === action.payload.id) {
-            item.quantity += action.payload.quantity
-            return item
+            item.quantity += action.payload.quantity;
+            return item;
           }
-          return item
-        })
+          return item;
+        });
         const newtotal = itemsUpdated.reduce(
           (acc, current) => (acc += current.price * current.quantity),
           0
-        )
-        console.log('este es el tota', newtotal)
+        );
+        console.log("este es el total", newtotal);
         return {
           ...state,
           items: itemsUpdated,
           total: 10,
           updatedAt: new Date().toLocaleString(),
-        }
+        };
       } else {
-        state.items.push(action.payload)
+        state.items.push(action.payload);
         const new2total = state.items.reduce(
           (acc, current) => (acc += current.price * current.quantity),
           0
-        )
+        );
 
-        console.log('este es el total 2', new2total)
+        console.log("este es el total 2", new2total);
         return {
           ...state,
           total: 20,
           updatedAt: new Date().toLocaleString(),
-        }
+        };
       }
     },
     removeItem: (state, action) => {},
   },
-})
+});
 
-export const { addItem, removeItem } = cartSlice.actions
+export const { addItem, removeItem } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
-export const baseUrl = 'https://e-comerce-8e6bb-default-rtdb.firebaseio.com'
-export const apiKey = 'AIzaSyBvjmwsWgLKe-YMaEAsSurX0M2gwW1rCMg'
-export const authUrl = 'https://identitytoolkit.googleapis.com/v1/'
+export const baseUrl = "https://prueba-5fb80-default-rtdb.firebaseio.com/";
+export const apiKey = "AIzaSyA9NMVTAkuC4bc-TDJFJTQS9t-l9m28ZtM";
+export const authUrl = "https://identitytoolkit.googleapis.com/v1/";
 export const googleApi = {
-  mapStatic: 'AIzaSyAjdhY5I6NmQDLdwsF6tas1br9FLnvNd3U',
-}
+  mapStatic: "AIzaSyAjdhY5I6NmQDLdwsF6tas1br9FLnvNd3U",
+};
